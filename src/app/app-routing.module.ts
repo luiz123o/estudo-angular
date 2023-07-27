04a4e156dc8c331abf63aafa97485e355be66ee9
Luiz Otavio <luiz123o@hotmail.com>
2023-07-27 14:25:56 -0300

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { authGuard } from './guards/auth.guard';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent, canActivate: [authGuard]
+  },
+  {
+    path: '', component: AuthComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
